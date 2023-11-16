63f4be302349d67b960b9c0c1419c166547b87cc
juanosarg <31105173+juanosarg@users.noreply.github.com>
2023-11-16 11:36:18 +0100

    v1.0

---
+++ 1.4/Source/VFEProps/VFEProps/Comps/CompProperties_ConstantFireOverlay.cs
@@ -0,0 +1,22 @@
+﻿
+using RimWorld;
+using UnityEngine;
+using Verse;
+namespace VFEProps
+{
+    public class CompProperties_ConstantFireOverlay : CompProperties_FireOverlay
+    {
+       
+
+        public int upOffset = 0;
+
+        public bool bigGraphic = false;
+
+        public CompProperties_ConstantFireOverlay()
+        {
+            compClass = typeof(CompConstantFireOverlay);
+        }
+
+       
+    }
+}
