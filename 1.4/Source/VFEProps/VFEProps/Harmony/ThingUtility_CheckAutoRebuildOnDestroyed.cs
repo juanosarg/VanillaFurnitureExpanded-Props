f7e0f008e8b600c642bb0ce115faa4adf986b1ba
SargBjornson <sarg.auxiliar@gmail.com>
2023-11-25 22:13:55 +0100

    v1.0

---
+++ 1.4/Source/VFEProps/VFEProps/Harmony/ThingUtility_CheckAutoRebuildOnDestroyed.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using Verse;
+using RimWorld.BaseGen;
+
+
+
+namespace VFEProps
+{
+
+
+    [HarmonyPatch(typeof(ThingUtility))]
+    [HarmonyPatch("CheckAutoRebuildOnDestroyed")]
+
+
+    public static class VFEProps_ThingUtility_CheckAutoRebuildOnDestroyed_Patch
+    {
+
+
+        [HarmonyPrefix]
+        public static bool NoAutoRebuildProps(Thing thing)
+
+        {
+            if (thing.def.defName.Contains("VFEPD_"))
+            {
+                return false;
+            }
+            return true;
+
+
+
+        }
+
+    }
+
+
+}
