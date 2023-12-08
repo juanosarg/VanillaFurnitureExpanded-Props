ab6a96dce07867cbaf413f234b93ba5b0b021dba
SargBjornson <sarg.auxiliar@gmail.com>
2023-12-08 23:43:44 +0100

    1.0

---
+++ 1.4/Source/VFEProps/VFEProps/Building/Building_VehicleWithTurret.cs
@@ -0,0 +1,67 @@
+﻿
+using RimWorld;
+using Verse;
+using Verse.Sound;
+
+using UnityEngine;
+using static HarmonyLib.Code;
+using System;
+
+namespace VFEProps
+{
+    public class Building_VehicleWithTurret : Building_SubstractsSilver
+    {
+        public DrawTurretExtension extension = null;
+
+        public Graphic graphic = null;
+
+        public DrawTurretExtension GetExtension
+        {
+            get
+            {
+                if (extension is null)
+                {
+                    extension = this.def.GetModExtension<DrawTurretExtension>();
+                   
+                }
+                return extension;
+            }
+        }
+
+        public Graphic GetGraphic
+        {
+            get
+            {
+                if (graphic is null)
+                {
+                    LongEventHandler.ExecuteWhenFinished(delegate { GetGraphicLong(); });
+                    
+                }
+                return graphic;
+            }
+        }
+
+        public void GetGraphicLong()
+        {
+            try
+            {
+                graphic = (Graphic_Single)GraphicDatabase.Get<Graphic_Single>(GetExtension.turretToDraw, ShaderDatabase.CutoutComplex, GetExtension.drawSize, this.def.graphicData.color);
+            }
+            catch (Exception) {  }
+        }
+
+        public override void Draw()
+        {
+            base.Draw();
+            var vector = this.DrawPos + Altitudes.AltIncVect;
+            vector.y += 5;
+            vector.x += GetExtension.offset.x;
+            vector.z += GetExtension.offset.y;
+            GetGraphic?.DrawFromDef(vector, this.Rotation, null);
+
+        }
+
+
+
+    }
+}
---
+++ 1.4/Source/VFEProps/VFEProps/DefExtensions/DrawTurretExtension.cs
@@ -0,0 +1,16 @@
+﻿using Verse;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VFEProps
+{
+
+    public class DrawTurretExtension : DefModExtension
+    {
+
+        public string turretToDraw;
+        public Vector2 drawSize;
+        public Vector2 offset;
+    }
+
+}
